b12acded7e36fde076732f11c53bc1c0ab7501e1
Phrygien <84671612+phrygien@users.noreply.github.com>
2024-04-16 21:00:27 +0300

    setup sidebar

--- resources/views/components/nav-link-app.blade.php
+++ resources/views/components/nav-link-app.blade.php
@@ -2,7 +2,7 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'flex items-center gap-x-3.5 py-2 px-2.5 bg-gray-200 text-sm text-rose-800 rounded-lg hover:bg-rose-100 dark:bg-rose-700 dark:text-white'
+            ? 'flex items-center gap-x-3.5 py-2 px-2.5 bg-rose-100 text-sm text-rose-800 rounded-lg hover:bg-rose-100 dark:bg-rose-700 dark:text-white'
             : 'flex items-center gap-x-3.5 py-2 px-2.5 bg-gray-100 text-sm text-neutral-700 rounded-lg hover:bg-gray-100 dark:bg-neutral-700 dark:text-white';
 @endphp
 
