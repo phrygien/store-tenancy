b7daebcabae23ea0b84932a505473fabd6632212
Phrygien <84671612+phrygien@users.noreply.github.com>
2024-04-19 22:37:33 +0300

    creation formulaire

--- resources/views/livewire/school/ecole-create.blade.php
+++ resources/views/livewire/school/ecole-create.blade.php
@@ -79,9 +79,9 @@
                             <input type="text" hidden ="hidden" wire:model='is_active'/>
                         </div>
                     </div>
-                    <button type="submit" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
-                        Add product
-                    </button>
+                    <x-ts-button wire:click="saveEcole" loading="saveEcole">
+                        Save data
+                    </x-ts-button>
                 </form>
             </div>
 
